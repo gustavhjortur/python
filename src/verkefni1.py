17484c8f49aaa842a3adea321d7613b4621d31f2
gustavhjortur <gustav.hjortur@gmail.com>
2016-11-28 21:59:43 +0000

    2. afangi

--- src/verkefni1.py
+++ src/verkefni1.py
@@ -16,10 +16,27 @@ def mod_sum(n):
     return summa
 
 #Write a function remove_empty that takes a list of strings and returns the same list, where empty strings have been removed.
-def remove_empty( *strings ):
+def remove_empty( strings ):
     print(strings)
     list = []
     for x in strings:
-        list.append(x)
+        #list.append(x)
+        if ( x != '' ):
+            list.append(x)
     return list
 
+#Write a function reverse_words(s) that takes a string as a parameter. The string contains a single line (i.e. the string contains no \n). The function returns the string s, where the order of the words in the string have been reversed. The words are separated by single space. 
+def reverse_words(s):
+    if s == '' or s == ' ':
+        return ''
+    listi = []
+    listi = s.split(' ')
+    i = int()
+    st = str()
+    i = -1
+    while ( i >= ( len(listi) * -1 ) ):
+        st += listi[i]
+        st += ' '
+        i -= 1
+    print( st.split() )
+    return st
