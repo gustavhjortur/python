a5e87cce83a69cf4bc40bc3f5047935084c6a7f0
gustavhjortur <gustav.hjortur@gmail.com>
2016-12-11 20:39:13 +0000

    minor mods and housekeeping, on more commit to go

--- src/verkefni4.py
+++ src/verkefni4.py
@@ -7,7 +7,7 @@ from shutil import copyfile
 from shutil import move
 
 # Following used while testing to start with empty structure folder
-os.system('rm -rf data/structured/*')
+#os.system('rm -rf data/structured/*')
 
 '''
 Funtion to take list of "video-files" in directory tree: inFolder
@@ -15,9 +15,13 @@ and try to figure out how to organice them into structured tree: outFolder
 exit code: 0 == Normal
 exit code: 1 == Read folder does not excist
 
+This can be downloaded from:
+https://github.com/gustavhjortur/python/tree/master/src/verkefni4
+
 ToDo
-- fix use of path names
+- fix use of path names, each spot has comment: #ToDo fix pathname
 - impliment argparser
+- delete empty directoryes
 '''
 def notMovieOrTv(name):
     if ( name.lower().endswith('.mp3') ):
@@ -31,7 +35,9 @@ def notMovieOrTv(name):
          name.lower().endswith('.gif') ):
         return 3
     if ( 'orrent' in name or
-         name.lower().endswith('.nfo') ):
+         name.lower().endswith('.nfo') or
+         name.lower().endswith('.mta') or
+         name.lower().endswith('.part') ):
         return 99
     return 0
 
@@ -50,43 +56,33 @@ def parsNames(name):
             return 'Pictures'
         if ( otherType == 99 ):
             return 'Trash'
-        #baseName = 'Music'
-        #newName = baseName + seriesName
         return 'unknown'
     elif ( '#' in name ):
-        print( '# ',  name.split('.#')[0], ' ', name[(name.index('#')+1):(name.index('#')+3)] )
         baseName = name.split('.#')[0]
         newName = baseName + seriesName
     elif ( 'S' in name ):
-        #print('\t\tS found ', name)
-        #print( 'S ',  name.split('.S')[0], ' ', name[(name.index('S')+1):(name.index('S')+3)] )
-        #print( name.replace(' ', '_') )
         try:
+            #ToDo fix pathname
             seriesName = '/Series' + str(int(name[(name.index('.S')+2):(name.index('.S')+4)]))
         except:
             try:
+                #ToDo fix pathname
                 seriesName = '/Series' + str(int(name[(name.index('.S')+2):(name.index('.S')+3)]))
             except:
                 pass
         if ( seriesName ):
             baseName = (name.split('.S')[0]).title()
             return baseName + seriesName
-        #if ('S' in name) or ( ('s' + name[name.index('s')+1] ) in name):
     elif ( '.s' in name ):
         if ( (name[(name.index('.s')+2):(name.index('.s')+4)] ).isdigit ):
-            #print('\n', name, '\n')
             for x in name.split('.s'):
                 try:
-                    #print( name[(name.index('s')+1):(name.index('s')+3)] )
+                    #ToDo fix pathname
                     seriesName = '/Series' + str(int(name[(name.index('.s')+2):(name.index('.s')+4)]))
-                    #seriesName = '/Series' + str(int(name[(name.index('s')+1):(name.index('s')+3)]))
-                    #print('\t\t\t', seriesName)
                 except:
                     try:
-                        #print( name[(name.index('s')+1):(name.index('s')+3)] )
+                        #ToDo fix pathname
                         seriesName = '/Series' + str(int(name[(name.index('.s')+2):(name.index('.s')+3)]))
-                        #seriesName = '/Series' + str(int(name[(name.index('s')+1):(name.index('s')+3)]))
-                        #print('\t\t\t', seriesName)
                     except:
                         pass
             if ( seriesName ):
@@ -94,80 +90,44 @@ def parsNames(name):
                 return baseName + seriesName
     elif ( 'x' in name ):
         for x in name.split('x'):
-            print(x)
             if ( (name[(name.index('x')+1):(name.index('x')+3)] ).isdigit() ):
-                print('\t', name[(name.index('x')-2):(name.index('x')+3)], ' ', name, ' ', name.index('x'))
-                print( '\t\t\t', name[name.index('x')-2:name.index('x')+3] )
                 try:
+                    #ToDo fix pathname
                     seriesName = '/Series' + str(int(name[name.index('x')-2:name.index('x')]))
                     seperator = str(int(name[name.index('x')-2:name.index('x')]))
-                    #numb = int( name[name.index('x')-2:name.index('x')] )
-                    #print('\t\t\t\t first')
                 except:
                     try:
+                        #ToDo fix pathname
                         seriesName = '/Series' + str(int(name[name.index('x')-1:name.index('x')]))
                         seperator = str(int(name[name.index('x')-1:name.index('x')]))
-                        #numb = int( name[name.index('x')-1:name.index('x')] )
-                        #print('\t\t\t\t seccond')
                     except:
                         pass
             if ( seriesName ):
                 baseName = (name.split(str(seperator+'x'))[0]).title()
-                print( '\t\t', baseName[:-1], ' ', seriesName, '\tNafnid mitt')
                 return baseName[:-1] + seriesName
-                #for x in name.split('.'):
-                #print( '\t\t', baseName )
-    #print()
     return newName
 
-def findAllFiles(inFolder, outFolder, fileDict=dict()):
-    f = []
-    d = []
-    if len(fileDict) != -1:
-        for (dirpath, dirnames, filenames) in walk(inFolder):
-            f.extend(filenames)
-            d.extend(dirnames)
-            #print('\t\t', dirnames, 'asd')
-            for x in dirnames:
-                findAllFiles( (inFolder + x), outFolder, fileDict )
-            #dp.extend(dirpath)
-            #print(filenames, '\t', inFolder, dirnames)
-            lastDirectory = ''
-            for x in filenames:
-                #print( inFolder, ' ', x , ' ',parsNames(x) )
-                newDirectory = parsNames(x)
-                if (newDirectory == 'useLast' ):
-                    newDirectory = lastDirectory
-                directory = (outFolder + newDirectory)
-                if not os.path.exists(directory):
-                    os.makedirs(directory)
-                # move or copy, select wisely
-                #print(x, ' ', directory)
-                # ToDo fix pathname slash for multi platform combatabilety
-                copyfile( (inFolder + '/' + x), (directory + '/' + x) )
-                #move( (inFolder + '/' + x), (directory + '/' + x) )
-                lastDirectory = newDirectory
-            break
-    else:
-        for (dirpath, dirnames, filenames) in walk(inFolder,'/',str(x for x in d)):
-            f.extend(filenames)
-            d.extend(dirnames)
-            for x in dirnames:
-                findAllFiles( (inFolder + x), fileDict)
-            #dp.extend(dirpath)
-            print(filenames, '\t',inFolder, dirnames)
-            break
-    #print()
-    #print(len(f))
-    for i in f:
-        if fileDict.get( i ):
-            #raise ItemExcistsError
-            #fileDict[i] = tmp
-            print( i, ' Already excists...')
-            #print( '\t\t\t\tend ', i.endswith('.png') )
-        else:
-            fileDict[i] = inFolder
-    return fileDict
+def findAllFiles(inFolder, outFolder, counter=0 ):
+    for (dirpath, dirnames, filenames) in walk(inFolder):
+        for x in dirnames:
+            counter = findAllFiles( (inFolder + x), outFolder, counter )
+        lastDirectory = ''
+        for x in filenames:
+            newDirectory = parsNames(x)
+            if (newDirectory == 'useLast' ):
+                newDirectory = lastDirectory
+            directory = (outFolder + newDirectory)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            # ToDo fix pathname slash for multi platform combatabilety
+            copyfile( (inFolder + '/' + x), (directory + '/' + x) )
+            #move( (inFolder + '/' + x), (directory + '/' + x) )
+            counter += 1
+            #print( '-', counter, '-', end="")
+            lastDirectory = newDirectory
+        break
+    return counter
+
 def printInstructions():
     print('''
     To use this script run: "./clean $NAME_OF_FILE_FOLDER $NAME_OF_DESTINATION"
@@ -187,7 +147,6 @@ def clean(args):
     except:
         printInstructions()
         return 1
-    #if ( os.path.isdir(inFolder) != True):
     if ( os.path.isdir(inFolder) != True):
         print(inFolder, ' Is not a folder')
         return 1
@@ -196,21 +155,15 @@ def clean(args):
     if ( os.path.isdir(outFolder) != True):
         print(outFolder, ' is not a folder or could not been created')
         return 1
-
-    #onlyfiles = [f for f in listdir(inFolder) if isfile(join(inFolder, f))]
-    #print(onlyfiles, 'lengd: ',len(onlyfiles) )
-    #print()
-    list = ( findAllFiles(inFolder, outFolder) )
-    print()
-    #print( list, '\n', len(list) )
-    print( len(list) )
+    counter = findAllFiles(inFolder, outFolder)
+    print( counter, 'files moved' )
     return 0
 
 
 
 
 
-#os.system('rm -rf data/structured/*')
-#clean(sys.argv)
+#For test in "ide" (like idle) lin 2 or 3 below kan be used
+clean(sys.argv)
 #clean(['runner', 'data/Down/', 'data/structured/']) #62 files
-clean(['runner', 'data/Download/', 'data/structured/']) #5198 files
+#clean(['runner', 'data/Download/', 'data/structured/']) #5198 files
